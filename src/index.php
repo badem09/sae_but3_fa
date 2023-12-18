6ba07a0043cb3eaea3511c5b24fa5a9e0f7b9c7c
Twip4 <paulbaudinot@gmail.com>
2023-12-18 14:40:09 +0100

    Home page

--- src/index.php
+++ src/index.php
@@ -1,2 +1,19 @@
-<?php
-echo "hello world";
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <script src="scripts/script.js"></script>
+</head>
+<body>
+<header>
+        <h1>SAE BUT</h1>
+</header>
+        <div class="Cards">
+            <div class="Card">
+                <img src="img/Ping.png" alt="Image 1">
+                <p>Module ping</p>
+            </div>
+        </div>
+</html>
