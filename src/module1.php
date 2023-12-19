52ad879d42094390ed4ebfcf942edaa1485af4c0
Twip4 <paulbaudinot@gmail.com>
2023-12-19 13:01:38 +0100

    Upgrade ping css and fix bugs

--- src/module1.php
+++ src/module1.php
@@ -1,36 +1,42 @@
 <!doctype html>
 <html lang="fr">
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<link rel="stylesheet" href="styles/styleIpv4.css">
 
 	<?php require("header.php");?>
 	
-	<div class="content">
-	<h2> Module 1 : Ping d'une adresse Ipv4 </h2>
-		<label for='adr_ip'> Adresse Ip à ping : </label>
-		<input type="text" id="input_adr_ip" required/>
-		<br>
-		<label for='adr_ip'> Nombre de paquets à envoyer </label>
-		<input id="input_nb_paquets" type="number" min="1" max="100"/> 
-		<br>
-		<label for='input_continu'> En continu  </label>
-		<input id="input_continu" type="checkbox"/> 
-		<br>
-		<button id='btn_adr_ip'> Envoyer </button>
-		
+	<div class="main">
+	<p> Module 1 : Ping d'une adresse Ipv4 </p>
+        <div id="subnet-form">
+            <div class="input_form">
+                <label for='adr_ip'> Adresse Ip à ping : </label>
+                <input type="text" id="input_adr_ip" required>
+            </div>
+            <div class="input_form">
+                <label for='adr_ip'> Nombre de paquets à envoyer </label>
+                <input id="input_nb_paquets" type="number" min="1" max="100">
+            </div>
+            <div class="input_form">
+                <label for='input_continu'> En continu  </label>
+                <input id="input_continu" type="checkbox">
+            </div>
+            <div class="input_form">
+                <button id='btn_adr_ip'> Envoyer </button>
+            </div>
+        </div>
 		<div id='ping_res'>
 			 <table id="pingTable">
 				<thead>
 				    <tr>
-				        <th>Timestamp</th>
-				        <th>Result</th>
+				        <td>Timestamp</td>
+				        <td>Result</td>
 				    </tr>
 				</thead>
 				<tbody id="pingTableBody"></tbody>
 			</table>
 		</div>
 	</div>
-	
-	<?php require("footer.php");?>
+
 <script> 
     function showImage() {
         var imageContainer = document.getElementById("ping_res");
@@ -60,12 +66,19 @@
         pingTableBody.innerHTML = ''; // Clear the content of the table body
     }
 
+    function isURL(str) {
+        const urlPattern = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*\/?$/;
+
+        return urlPattern.test(str);
+    }
+
+
     $('body').on('click', '#btn_adr_ip', function (){
 		resetTable();
     	$adr_ip = document.getElementById("input_adr_ip").value
     	$nb_paquets = document.getElementById("input_nb_paquets").value
     	var continu_checkbox = document.getElementById("input_continu")
-    	if (isValidIPAddress($adr_ip)){
+    	if (isValidIPAddress($adr_ip) || isURL($adr_ip)){
 	    	var url = "module1_ping.php?adr_ip="+$adr_ip + "&nb_paquets=" + $nb_paquets;
 	    	if (continu_checkbox.checked){
 				url += "&continu=True";
