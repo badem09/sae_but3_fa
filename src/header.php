ad9f00ce23bad5d36c34462242a695ec433b8a08
Twip4 <paulbaudinot@gmail.com>
2023-12-19 19:37:42 +0100

    Page titre modif

--- src/header.php
+++ src/header.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-    <title>Titre de la page</title>
+    <title>SAÉ de Noël</title>
     <link rel="stylesheet" href="styles/style.css">
     <script src="scripts/script.js"></script>
 </head>
