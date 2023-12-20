c0aad0831082703b511f252cfdb81ce92a630c85
Demba <badembajob@gmail.com>
2023-12-20 13:06:55 +0000

    Premiere version fonctionnelle a tester

--- src/ping_action.php
+++ src/ping_action.php
@@ -6,7 +6,7 @@ header('Connection: keep-alive');
 $adr_ip = $_GET['adr_ip'];
 $nb_paquets = $_GET['nb_paquets'];
 $continu = isset($_GET['continu']) && $_GET['continu'] === 'True';
-
+$accessed = False;
 $command = "ping -c $nb_paquets $adr_ip";
 if ($continu) {
     $command = "ping $adr_ip";
@@ -23,14 +23,20 @@ $process = proc_open($command, $descriptorspec, $pipes);
 if (is_resource($process)) {
     stream_set_blocking($pipes[1], 0); // Set non-blocking mode for stdout
     stream_set_blocking($pipes[2], 0); // Set non-blocking mode for stderr
+    $start_time = time(); // Record the start time
 
     while (true) {
         $line = fgets($pipes[1]); // Read a line from the process output
-
         if ($line !== false) {
-            // Check for the presence of "PING" to exclude the first line
-            if (strpos($line, "PING") === false) {
-                echo "data: $line\n\n";
+            $accessed = True;
+            if (strpos($line, "PING") === false) { // To exclude the first line of output
+                if (strpos($line, "ping statistics") !== false ||
+                    strpos($line, "packets transmitted") !== false ||
+                    strpos($line, "rtt min/avg/max/mdev") !== false) {
+                    echo "data: data_resume:$line\n\n"; // Send summary statistics to the client
+                } else {
+                    echo "data: data_table:$line\n\n"; // Send regular data to the client
+                }
                 ob_flush();
                 flush();
             }
@@ -42,14 +48,18 @@ if (is_resource($process)) {
             break;
         }
 
-//        usleep(1000); // Sleep for 1ms (adjust as needed)
-
         // Check for client disconnect
         if (connection_aborted()) {
             break;
         }
+        // If the ping has been running for more than 10 seconds : Ip unreachable
+        if (!$accessed && time() - $start_time > 10) {
+            echo "data: data_unvalid_ip:L'adresse : $adr_ip à l'air d'être inaccessible. \n\n";
+            ob_flush();
+            flush();
+            break;
+        }
     }
-
     fclose($pipes[1]);
     fclose($pipes[2]);
     proc_close($process);
