37a2dee1a7c4f42c713f89cff296755d781b4719
badem09 <badembajob@gmail.com>
2023-12-21 13:19:47 +0000

    Gestion du cas ou ping genere une erreur (stderr)

--- src/ping_action.php
+++ src/ping_action.php
@@ -6,7 +6,7 @@ header('Connection: keep-alive');
 $adr_ip = $_GET['adr_ip'];
 $nb_paquets = $_GET['nb_paquets'];
 $continu = isset($_GET['continu']) && $_GET['continu'] === 'True';
-$accessed = False;
+$accessed = false;
 $command = "ping -c $nb_paquets $adr_ip";
 if ($continu) {
     $command = "ping $adr_ip";
@@ -19,41 +19,60 @@ $descriptorspec = array(
 );
 
 $process = proc_open($command, $descriptorspec, $pipes);
-//To avoid time and reptetition restriciton (sur le nombre de ping)
-set_time_limit(0); 
+
+// To avoid time and repetition restrictions (sur le nombre de ping)
+set_time_limit(0);
 ob_end_flush();
 
 if (is_resource($process)) {
     stream_set_blocking($pipes[1], 0); // Set non-blocking mode for stdout
     stream_set_blocking($pipes[2], 0); // Set non-blocking mode for stderr
     $start_time = time(); // Record the start time
+    $stderr_content = ''; // Variable to store stderr content
 
     while (true) {
-        $line = fgets($pipes[1]); // Read a line from the process output
-        if ($line !== false) {
-            $accessed = True;
-            if (strpos($line, "PING") === false) { // To exclude the first line of output
-                if (strpos($line, "ping statistics") !== false ||
-                    strpos($line, "packets transmitted") !== false ||
-                    strpos($line, "rtt min/avg/max/mdev") !== false) {
-                    echo "data: data_resume:$line\n\n"; // Send summary statistics to the client
-                } else {
-                    echo "data: data_table:$line\n\n"; // Send regular data to the client
-                }
-                ob_flush();
-                flush();
+        // Read from both stdout and stderr
+        $line_stdout = fgets($pipes[1]);
+        $line_stderr = fgets($pipes[2]);
+
+        if ($line_stdout !== false) {
+            $accessed = true;
+	    if (strpos($line_stdout, 'PING') === false){
+            if (strpos($line_stdout, "ping statistics") !== false ||
+                strpos($line_stdout, "packets transmitted") !== false ||
+                strpos($line_stdout, "rtt min/avg/max/mdev") !== false) {
+                echo "data: data_resume:$line_stdout\n\n"; // Send summary statistics to the client
+            } else {
+                echo "data: data_table:$line_stdout\n\n"; // Send regular data to the client
             }
+            flush();
+            ob_flush();
+            }
+ 	}
+ /*       if ($line_stderr !== false){ //&& strpos($line_stderr, "No address associated with hostname") !== false) {
+            // Send an error message related to hostname resolution failure
+            echo "data: data_error:".$line_stderr." \n\n";
+            ob_flush();
+            flush();
+            break;
+        }*/
+        if ($line_stderr !== false) {
+            // Collect stderr content
+            $stderr_content .= $line_stderr;
         }
+
         // Check for process termination
         $status = proc_get_status($process);
         if (!$status['running']) {
             break;
         }
+
         // Check for client disconnect
         if (connection_aborted()) {
             break;
         }
-        // If the ping has been running for more than 10 seconds : Ip unreachable
+
+        // If the ping has been running for more than 10 seconds
         if (!$accessed && time() - $start_time > 10) {
             echo "data: data_unvalid_ip:L'adresse : $adr_ip à l'air d'être inaccessible. \n\n";
             ob_flush();
@@ -61,6 +80,14 @@ if (is_resource($process)) {
             break;
         }
     }
+
+    // Send stderr content as a single message
+    if (!empty($stderr_content)) {
+        echo "data: data_error:" . $stderr_content . "\n\n";
+        ob_flush();
+        flush();
+    }
+
     fclose($pipes[1]);
     fclose($pipes[2]);
     proc_close($process);
