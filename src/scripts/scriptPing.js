5bc629b02b634a81455ee593a44688e1159f04f5
badem09 <badembajob@gmail.com>
2023-12-22 15:02:17 +0000

    Documentation des fonctions et commentaires

--- src/scripts/scriptPing.js
+++ src/scripts/scriptPing.js
@@ -1,48 +1,69 @@
-
+/**
+ * Affiche l'image de chargement
+ */
 function showImageLoad() {
     document.getElementById("div_load").innerHTML = '<img src="img/loading.webp" alt="Chargement">';
 }
 
+/**
+ * Cache l'image de chargement
+ */
 function hideImageLoad(){
     document.getElementById("div_load").innerHTML = '';
 }
 
-function isValidIPAddress(input) {
+/**
+ * Verifie si le parametre est une adresse IPv4 valide
+ * @param {string} adresse : Adresse a verifier  
+ */
+function isValidIPAddress(adresse) {
     var ipRegex = /^(\d{1,3}\.){3}\d{1,3}$/;
-    return ipRegex.test(input);
+    return ipRegex.test(adresse);
 }
 
-function isURL(str) {
+/**
+ * Verifie si le parametre est une url valide
+ * @param {string} url : URL a verifier  
+ */
+function isURL(url) {
     const urlPattern = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*\/?$/;
-    return urlPattern.test(str);
+    return urlPattern.test(url);
 }
 
+/**
+ * Vide le tableau affichant les pings
+ */
 function resetTable() {
     document.getElementById("pingTableBody").innerHTML = '';
 }
 
+/**
+ * Vide la div qui affiche le resume des ping
+ */
 function resetDivResume(){
     document.getElementById("div-resume").innerHTML = "";
 }
 
+/**
+ * Vide la div qui affiche les statistiques 
+ */
 function resetDivStat(){
     document.getElementById("ping-stats").innerHTML = "";
 }
 
 function updatePingStats(data,pingTimes) {
 
-    // Extract time from the data string (you may need to adjust this based on your ping output)
+    // recupere le temps du ping a partir de la data envoyee
     var timeMatch = data.match(/time=([\d.]+) ms/);
     if (timeMatch) {
         var pingTime = parseFloat(timeMatch[1]);
         pingTimes.push(pingTime);
     
-        console.log(pingTimes);
         var maxTime = Math.max(...pingTimes);
         var minTime = Math.min(...pingTimes);
         var avgTime = pingTimes.reduce((sum, time) => sum + time, 0) / pingTimes.length;
 
-        // Update HTML to display max, min, and avg
+        // Update la div stats avec les nouveaux  max, min, and avg
         var statsDiv = document.getElementById("ping-stats");
         statsDiv.innerHTML = `<p>Temps maximum: ${maxTime.toFixed(3)} ms</p>`;
         statsDiv.innerHTML += `<p>Temps minimum: ${minTime.toFixed(3)} ms</p>`;
@@ -50,11 +71,11 @@ function updatePingStats(data,pingTimes) {
     }
 }
 
-
+/**
+ * Ajoute une nouvelle ligne (td) au tableau des ping et scroll jusqu'en bas du tableau
+ * @param {string} data : 1 ligne de l'output de la commande correspondant a un ping   
+ */
 function updatePingTable(data) {
-    /**
-     * Adds a new td : timestamp, pingResult to the table 'pingTable' and scrolls to the bottom
-     */
     var pingTableBody = document.getElementById("pingTableBody");
     var row = document.createElement("tr");
     var timestampCell = document.createElement("td");
@@ -70,6 +91,10 @@ function updatePingTable(data) {
     pingResDiv.scrollTop = pingResDiv.scrollHeight; // scroll to the bottom
 }
 
+/**
+ * Retourne le numero du ping correspondant au parametre data
+ * @param {string} data : 1 ligne de l'output de la commande correspondant a un ping   
+ */
 function getNoPing(data){
     var match = data.match(/icmp_seq=(\d+)/);
     return match && parseInt(match[1], 10);
@@ -85,20 +110,19 @@ function updateDivResume(data, pingCounter) {
     divResume.innerHTML += "<p> Pourcentage d'echec: " + packetLoss.toFixed(2) + " %</p>";
 }
 
+/**
+ * Evite que l'utilisateur puisse a la fois entrer un nombre de paquet et appuyer
+ * sur la checkbox 'En continu' 
+ */
 function toggleInputs() {
     var nbPaquetsInput = document.getElementById("input_nb_paquets");
     var continuCheckbox = document.getElementById("input_continu");
 
-    // If "En continu" is checked, disable "Nombre de paquets"
+    // Si la checkbox "En continu" est validee, disable "Nombre de paquets"
     nbPaquetsInput.disabled = continuCheckbox.checked;
 
-    // If "Nombre de paquets" is filled out, disable "En continu"
+    // Si a remplit l'input "Nombre de paquets" , disable "En continu"
     continuCheckbox.disabled = nbPaquetsInput.value !== '';
-
-    // If "Nombre de paquets" is filled out, uncheck "En continu"
-    if (nbPaquetsInput.value) {
-        continuCheckbox.checked = false;
-    }
 }
 
 
@@ -139,14 +163,9 @@ $('body').on('click', '#btn_adr_ip', function (){
 	if (continu_checkbox.checked){
 	    url += "&continu=True";
 	}
-	//var url = "ping_action.php?adr_ip="+$adr_ip + "&nb_paquets=" + $nb_paquets;
-        //if (continu_checkbox.checked){
-        //    url += "&continu=True";
-        //}
         showImageLoad();
         eventSource = new EventSource(url);
         eventSource.onmessage = function (event) {
-            console.log("ok",event.data);
             hideImageLoad();
             if (event.data.startsWith("data_table:") && event.data.length > 11) { // ping
                 pingCounter++;
